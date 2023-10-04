9677689d79deb7a82c01f1b3398d72a6743937fb
nguyenngocthach1403 <146806999+nguyenngocthach1403@users.noreply.github.com>
2023-10-04 19:54:12 +0700

    botnav.dart

--- lib/botnav.dart
+++ lib/botnav.dart
@@ -1,1 +1,31 @@
+import 'package:demo_material_app/detail_screen.dart';
+import 'package:demo_material_app/index_screen.dart';
+import 'package:flutter/material.dart';
 
+class BottomMenu extends StatelessWidget {
+  const BottomMenu({super.key, required this.idx});
+  final idx;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: idx,
+      fixedColor: Colors.blueAccent,
+      items: const [
+        BottomNavigationBarItem(label: "Trang chủ", icon: Icon(Icons.home)),
+        BottomNavigationBarItem(label: "Chi tiết", icon: Icon(Icons.details))
+      ],
+      onTap: (int indexOfItem) {
+        if (indexOfItem == 0) {
+          Navigator.popUntil(context, (route) => route.isFirst);
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const IndexScreen()));
+        } else {
+          Navigator.popUntil(context, (route) => route.isFirst);
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const DetailScreen()));
+        }
+      },
+    );
+  }
+}
